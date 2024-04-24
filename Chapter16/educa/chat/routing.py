10a7f8fc9cf1b4f08b23b68812da3b3e2bf5f639
Antonio Melé <antonio.mele@gmail.com>
2024-04-24 10:43:12 +0200

    fixed linebreaks

--- Chapter16/educa/chat/routing.py
+++ Chapter16/educa/chat/routing.py
@@ -2,6 +2,7 @@ from django.urls import re_path
 
 from . import consumers
 
+
 websocket_urlpatterns = [
     re_path(
         r'ws/chat/room/(?P<course_id>\d+)/$',
--- Chapter16/educa/chat/urls.py
+++ Chapter16/educa/chat/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-app_name = 'chat'
 
+app_name = 'chat'
 
 urlpatterns = [
     path(
