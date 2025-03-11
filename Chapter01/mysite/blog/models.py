906fc1e3f899e677187444dd3ea25e7432bd1ce1
Mark Walker <mwalker@ismgames.com>
2025-03-11 01:02:12 +0000

    Extra favourite post bits

--- Chapter01/mysite/blog/models.py
+++ Chapter01/mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -48,3 +49,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.id,
+            ],
+        )
--- Chapter01/mysite/blog/urls.py
+++ Chapter01/mysite/blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
+    path('favrourite/add/<int:id>/', views.add_favourite, name='add_favourite'),
+    path('favourites/', views.favourites, name='favourites'),
 ]
